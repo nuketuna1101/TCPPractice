51b7323fba6873023b21cdad33b7d80784094077
Jaeyoung <jyko1101@gmail.com>
2025-01-07 15:17:46 +0900

    feat: user database

---
+++ src/db/user/user.db.js
@@ -0,0 +1,26 @@
+//====================================================================================================================
+//====================================================================================================================
+// user.db.js
+// pools 객체에 있는 USER_DB의 커넥션 풀을 통해 쿼리를 전송
+// uuid 라이브러리를 통해 id를 생성
+//====================================================================================================================
+//====================================================================================================================
+
+import { v4 as uuidv4 } from 'uuid';
+import pools from '../database.js';
+import { SQL_QUERIES } from './user.queries.js';
+
+export const findUserByDeviceID = async (deviceId) => {
+    const [rows] = await pools.USER_DB.query(SQL_QUERIES.FIND_USER_BY_DEVICE_ID, [deviceId]);
+    return row[0]
+};
+
+export const createUser = async (deviceId) => {
+    const id = uuidv4();
+    await pools.USER_DB.query(SQL_QUERIES.CREATE_USER, [id, deviceId]);
+    return { id, deviceId };
+};
+
+export const updateUserLogin = async (id) => {
+    await pools.USER_DB.query(SQL_QUERIES.UPDATE_USER_LOGIN, [id]);
+};
