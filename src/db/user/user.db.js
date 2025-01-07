565c824d95cf41bae977986675e246e6c5e42572
Jaeyoung <jyko1101@gmail.com>
2025-01-07 15:23:04 +0900

    refactor: userdb using converter util

--- src/db/user/user.db.js
+++ src/db/user/user.db.js
@@ -9,10 +9,11 @@
 import { v4 as uuidv4 } from 'uuid';
 import pools from '../database.js';
 import { SQL_QUERIES } from './user.queries.js';
+import { toCamelCase } from '../../utils/transformCase.js';
 
 export const findUserByDeviceID = async (deviceId) => {
     const [rows] = await pools.USER_DB.query(SQL_QUERIES.FIND_USER_BY_DEVICE_ID, [deviceId]);
-    return row[0]
+    return toCamelCase(rows[0]);
 };
 
 export const createUser = async (deviceId) => {
